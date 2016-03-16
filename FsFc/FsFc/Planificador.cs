ba9eae631a4e3d966eb2b89533fcdbe285d76e6a
Arthyom <allpipiasaaa@hotmail.com>
2016-03-15 23:34:55 -0600

    Procesos y planificador

---
+++ FsFc/FsFc/Planificador.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FsFc
+{
+    public class Planificador
+    {
+        /* 1 ocupado, 0 libre */
+        private int estado;
+        public int GSestado
+        {
+            set
+            {
+                this.estado = value;
+            }
+            get
+            {
+                return this.estado;
+            }
+        }
+        public int rafagaCpu = 3;
+
+        public void planificarFcFs(Proceso pActual)
+        {
+            // procesar hasta que el proceso termine 
+            while (pActual.faltate >= 3)
+            {
+                pActual.faltate -= 3;
+                pActual.GSestado = 1;
+            }
+
+            pActual.GSestado = 4;
+        }
+    }
+}
---
+++ FsFc/FsFc/Proceso.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FsFc
+{
+    public class Proceso
+    {
+        private int estado;
+        public int GSestado
+        {
+            get
+            {
+                return this.estado;
+            }
+
+            set
+            {
+                this.estado = value;
+            }
+        }
+
+        private string nombre;
+        public string GSnombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+
+        private int duracion;
+        public int GSduracion
+        {
+            set
+            {
+                this.duracion = value;
+            }
+            get
+            {
+                return this.duracion;
+            }
+        }
+
+        public int faltate;
+
+        public Proceso()
+        {
+            /* 0-en espera, 1-en ejecucion 2- listo  -1- bloqueado 4-listo*/
+            this.estado = 0;
+            this.nombre = " ";
+            this.duracion = 0;
+            this.faltate = 10;
+        }
+
+
+    }
+}
